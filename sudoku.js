83ef57f242efe5d56bd0422fe7909d592f6d82d8
parkhomenkoNY <natalbka.p92@gmail.com>
2023-10-06 16:25:33 +0300

    getCoord

--- sudoku.js
+++ sudoku.js
@@ -1,15 +1,53 @@
 const fs = require("fs");
 
-function read() {
-  let sss = fs.readFileSync("./puzzles.txt","utf8").split("\n");
-  console.log(sss)
+function read(number = Math.floor(Math.random() * 5)) {
+  let sss = fs.readFileSync("./puzzles.txt","utf8").split("\r\n");
+  let arr =[];
+  arr.push(sss[number - 1])
+  let arr2 = [];
+  for(let i = 0; i < arr[0].length; i+=9){
+    arr2.push([arr[0].slice(i,i+9)]);
+  }
+  let arr3 = arr2.map((row) => row[0].split("").map((char)=>char));
+  return arr3;
+  
 }
-read();
+ console.table(read());
 
-let getRandomInteger = () => {
-  let sudokuNumber = Math.round(Math.random() * (5 -1 + 1) +1);
-  return (sudokuNumber);
-} 
+ let getSudokuNumber = () => {
+    let sudokuNumber = Math.round(Math.random() * (9 -1 + 1) +1);
+    return (sudokuNumber);
+  } 
+
+// function getCoord () {
+//   let array = read();
+//   let newArr =[];
+//   for (let i = 0; i < array.length; i++) {
+//     for (let j = 0; j < array.length; j++) {
+//       if(array[i][j] === "-") {
+//         newArr.push([i, j]);
+//       }  
+//     } 
+//   }
+//   return newArr;
+// }
+//console.log (getCoord());
+
+function getCoord () {
+  let array = read();
+  let newArr =[];
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if(array[i][j] === "-") {
+        a=([i, j]);
+        return a;
+      }  
+    } 
+  }
+  return null;
+}
+console.log (getCoord());
+
 
 function solve() {
   /**
